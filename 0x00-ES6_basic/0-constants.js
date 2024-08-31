e585d3ed71f08e89eb55199e381d6bd7b44b83be
Nosipho2021 <lorrainemdk@gmail.com>
2024-08-31 13:41:13 +0000

    instantiate variables using const and let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,14 @@
-// 0-constants.js
-
-// Function to instantiate a variable using const
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Function to return a string
+
 export function getLast() {
   return ' is okay';
 }
 
-// Function to instantiate a variable using let
+
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
